2c3404765d993d68452e89766d02cf4698cb1a5d
Ma'ruf Ma'arij <72728486+mrfzvx12@users.noreply.github.com>
2024-03-24 16:56:07 +0700

    update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function start(file) {
 	p.on("exit", (code) => {
 		isRunning = false;
 		console.error("Exited with code:", code);
-		if (code === 0) return;
+		if (code === 0 || code === 1) return;
 		fs.watchFile(args[0], () => {
 			fs.unwatchFile(args[0]);
 			start(file);
