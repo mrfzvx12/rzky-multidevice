a4e0ae9d27cb52fcbf336926ed2c4ae13747062d
Ma'ruf Ma'arij <72728486+mrfzvx12@users.noreply.github.com>
2024-07-25 22:44:21 +0700

    update index.js

--- index.js
+++ index.js
@@ -10,35 +10,40 @@ var isRunning = false;
  * @param {String} file `path/to/file`
  */
 function start(file) {
-	if (isRunning) return;
-	isRunning = true;
-	let args = [path.join(__dirname, file), ...process.argv.slice(2)];
-	let p = spawn(process.argv[0], args, {
-		stdio: ["inherit", "inherit", "inherit", "ipc"],
-	});
-	p.on("message", (data) => {
-		console.log("[RECEIVED]", data);
-		switch (data) {
-			case "reset":
-				p.kill();
-				isRunning = false;
-				start.apply(this, arguments);
-				break;
-			case "uptime":
-				p.send(process.uptime());
-				break;
-		}
-	});
-	p.on("exit", (code) => {
-		isRunning = false;
-		console.error("Exited with code:", code);
-		//if (code === 0 || code === 1) return;
-		// fs.watchFile(args[0], () => {
-		// 			fs.unwatchFile(args[0]);
-		start(file);
-		// 		});
-	});
-	// console.log(p)
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+  })
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
+    })
+  })
+  // console.log(p)
 }
 
 start("main.js");
